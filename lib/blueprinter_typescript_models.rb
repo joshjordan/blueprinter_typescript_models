a68a8ba13df0de7357520ce6a58f3cf0f3fdaf9a
Josh Jordan <josh.jordan@gmail.com>
2024-11-16 11:12:17 -0500

    bundle gem blueprinter_typescript_models --mit --test=rspec

---
+++ lib/blueprinter_typescript_models.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "blueprinter_typescript_models/version"
+
+module BlueprinterTypescriptModels
+  class Error < StandardError; end
+  # Your code goes here...
+end
